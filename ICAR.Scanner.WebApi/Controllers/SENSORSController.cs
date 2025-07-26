c7c1fbe930d54c42aa905dbd2803698b0aa8e139
@ranjan.senapati.ext <ranjan.senapati@siemens-energy.com>
2025-07-26 21:02:52 +0530

    changes

---
+++ ICAR.Scanner.WebApi/Controllers/SENSORSController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ICAR.Scanner.WebApi.Controllers
+{
+    public class SENSORSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
