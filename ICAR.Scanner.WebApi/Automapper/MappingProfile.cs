6e2026c4d593021b9d0f78ba5d1a39b900dc1250
@ranjan.senapati.ext <ranjan.senapati@siemens-energy.com>
2025-10-25 19:29:20 +0530

    sensor

--- ICAR.Scanner.WebApi/Automapper/MappingProfile.cs
+++ ICAR.Scanner.WebApi/Automapper/MappingProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using ICAR.Scanner.Models.DTOs;
 using ICAR.Scanner.DataAccess.Models;
+using ICAR.Scanner.Models.DTOs.Request;
 
 namespace ICAR.Scanner.WebApi.Automapper;
 
@@ -9,7 +10,15 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Tree, TreesDto>().ReverseMap();
+        CreateMap<TREESCreateDTO, Tree>();
+
         CreateMap<User, UserDTO>().ReverseMap();
         CreateMap<UserCreateDTO, User>();
+
+        CreateMap<SENSOR, SensorDTO>().ReverseMap();
+        CreateMap<SensorCreateDTO, SENSOR>();
+
+        CreateMap<AuditTree, AuditTreeDTO>().ReverseMap();
+        CreateMap<AuditTreeCreateDTO, AuditTree>();
     }
 }
